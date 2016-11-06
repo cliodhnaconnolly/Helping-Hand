80ec6b404a48f6b5abcee66d4072257aa12ed18c
cliodhnaconnolly <cliodhnaconnolly@hotmail.com>
2016-11-06 20:42:24 +0000

    First Commit

---
+++ HelpingHandAndroid/app/src/main/java/com/example/helpinghand/helpinghandandroid/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.helpinghand.helpinghandandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import static com.example.helpinghand.helpinghandandroid.R.id.browser;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView browser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        browser = (WebView)findViewById(R.id.browser);
+        browser.loadUrl("https://morning-eyrie-54020.herokuapp.com/");
+        browser.setWebViewClient(new WebViewClient());
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // Check if the key event was the Back button and if there's history
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && browser.canGoBack()) {
+            browser.goBack();
+            return true;
+        }
+        // If it wasn't the Back key or there's no web page history, bubble up to the default
+        // system behavior (probably exit the activity)
+        return super.onKeyDown(keyCode, event);
+    }
+}
